a11756dc66b79e734ff6a8643f3d6ef8938f8003
frederik <frederik@FrederikB.lan>
2015-11-01 12:08:27 +0100

    Felt opjekter oprettet og importeret til spilleplade klasse

--- src/spilleplade/controller/Spilleplade.java
+++ src/spilleplade/controller/Spilleplade.java
@@ -1,5 +1,24 @@
 package spilleplade.controller;
 
+import felt.entity.Felt;
+
 public class Spilleplade {
 
+	private  Felt f1 = new Felt();
+	private  Felt f2 = new Felt();
+	private  Felt f3 = new Felt();
+	private  Felt f4 = new Felt();
+	private  Felt f5 = new Felt();
+	private  Felt f6 = new Felt();
+	private  Felt f7 = new Felt();
+	private  Felt f8 = new Felt();
+	private  Felt  f9 = new Felt();
+	private  Felt f10 = new Felt();
+	private  Felt f11 = new Felt();
+
+	private int[] spilleplade = new int [11];
+	
+//	spilleplade[0] = f1;
+	
+	
 }

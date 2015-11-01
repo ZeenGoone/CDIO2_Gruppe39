53cd32c25b1052921afd1414cbb46a5dfd6f5732
frederik <frederik@FrederikB.lan>
2015-11-01 12:01:19 +0100

    Felt klasse oprettet med set & get metoder

---
+++ src/felt/entity/Felt.java
@@ -0,0 +1,16 @@
+package felt.entity;
+
+public class Felt {
+	
+	private int vaerdi;
+
+	
+	//Set & Get metode af felt vaerdi
+	public void setVaerdi ( int vaerdi ) {
+		this.vaerdi = vaerdi;
+	}
+	
+	public int getVaerdi () {
+		return vaerdi;
+	}
+}

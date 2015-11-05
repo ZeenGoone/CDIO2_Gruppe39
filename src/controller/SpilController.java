ab8e5610769f9b952e9f1be6a9068cbcc0a7e171
frederik <frederik@FrederikB.lan>
2015-11-05 10:25:55 +0100

    Fixet æøå i feltbeskrivelser, oprettet getNuvaerendespiller-metode i spilcontroller, bygget videre på spilRundemetode

--- src/controller/SpilController.java
+++ src/controller/SpilController.java
@@ -12,7 +12,7 @@ public class SpilController {
 	static Spiller s1 = new Spiller();	
 	static Spiller s2 = new Spiller();	
 	static Raflebaeger raflebaeger = new Raflebaeger();
-	private static int nuvarendespiller = 0;
+	private static int nuvarendespiller = 1;
 
 	public static void main(String[] args) {
 		sp = new Spilleplade();
@@ -22,21 +22,19 @@ public class SpilController {
 
 
 		//Mens vinder ikke er fundet
-		while(s1.getBeholdning() < 1200 || s2.getBeholdning() < 1200) {
+		while(vinder == false) {
 			if ( nuvarendespiller == 1) {
-				spilRunde(s1);
 				nuvarendespiller = 2;
+				spilRunde(s1);
 			}
 			else {
 				spilRunde(s2);
 				nuvarendespiller = 1;
 			}
 			if (vinder == false) {
-				GUI.getUserButtonPressed("Spiller " + nuvarendespiller + "s tur", "Tryk for at kaste!");
+				GUI.getUserButtonPressed("Spiller " + nuvarendespiller + "s tur", "Tryk for at kaste!");	
 			}
-			else if ( vinder == true) {
-				GUI.getUserButtonPressed("Spiller " + nuvarendespiller + " har vundet gz");
-			}
+			
 		}
 
 	}
@@ -47,14 +45,33 @@ public class SpilController {
 		raflebaeger.slaaTerninger();
 		
 		//Lægger feltets værdi til spillers beholdning
-		s.opdaterBeholdning(sp.getFelt(raflebaeger.getSum()).getVaerdi());
-		
+		s.opdaterBeholdning(sp.getFelt(raflebaeger.getSum()-2).getVaerdi());
 		gc.opdaterSpillerScore(s);
-
-
+		
+		gc.visBesked(sp.getFelt(raflebaeger.getSum()-2).getBeskrivelse());
+		
+		if ( raflebaeger.getSum() == 10 ) {
+			getNuvaerendespiller( nuvarendespiller );
+		}	
+		
+		
+		if ( s.getBeholdning() >= 1200) {
+			getNuvaerendespiller( nuvarendespiller );
+			GUI.getUserButtonPressed("Spiller " + nuvarendespiller + " har vundet gz", "Fedt spil!");
+			gc.afslutSpil();
+			
+			//vinder = true;
+		}
 	}
 
 	public void flytBrik(int a, String b){
 
+	}
+	
+	public static void getNuvaerendespiller ( int nuvaerendespiller ) {
+		if ( nuvaerendespiller == 1) 
+			nuvarendespiller =  2;
+		else if ( nuvaerendespiller == 2)
+			nuvarendespiller = 1;
 	}
 }

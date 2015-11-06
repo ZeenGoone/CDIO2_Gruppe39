d11534d06c4381e3692e5201fbee5c7b69c63d38
zeengoone <s974489@student.dtu.dk>
2015-11-06 10:41:32 +0100

    Lavet testNegativ

--- src/test/testNegativBeholdning.java
+++ src/test/testNegativBeholdning.java
@@ -4,11 +4,21 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+import entity.Spiller;
+
 public class testNegativBeholdning {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		
+		// instantierer en spiller
+		Spiller spiller = new Spiller();
+		
+		// kører testen 100000 gange med et tilfældigt negativt tal
+		for(int i = 0;i < 100000;i++){
+			spiller.opdaterBeholdning((int) (Math.random()*-10000));
+			// sikrer at værdien stadig er 0 uanset hvilket negativt tal der bliver prøvet
+			assertEquals(0, spiller.getBeholdning());
+		}
 	}
-
 }

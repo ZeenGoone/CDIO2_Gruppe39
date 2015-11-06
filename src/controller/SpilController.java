f4e3e4ad62d2bbbf761ec7345b307661c0508ad7
zeengoone <s974489@student.dtu.dk>
2015-11-06 08:11:49 +0100

    Opdateret til Objekt-optimeret istedet for så meget med variable

--- src/controller/SpilController.java
+++ src/controller/SpilController.java
@@ -6,17 +6,18 @@ import entity.Spiller;
 
 public class SpilController {
 
-	//initialiserer de noedvendige objekter
+	// initialiserer attributter
 	private static Spilleplade sp;
 	private static GUIcontroller gc;
 	private static Spiller s1;
 	private static Spiller s2;
 	private static Raflebaeger raflebaeger;
+	private static Spiller nuvaerendeSpiller;
 	
-	private static int nuvarendespiller = 0;
 	private static boolean vinder = false;
 	
 	public static void main(String[] args) {
+		// initialiserer variable
 		sp = new Spilleplade();
 		gc = new GUIcontroller(sp);
 		raflebaeger = new Raflebaeger();
@@ -24,39 +25,21 @@ public class SpilController {
 		s2 = new Spiller();
 		gc.startSpil();
 		
+		nuvaerendeSpiller = hvemStarter();
 		
-		//afgoer hvem der starter via math.random
-		int hvemStarter = (int) (Math.random()*2)+1 ;
-		
-		if(hvemStarter == 1 ){
-			nuvarendespiller = 1;
-			gc.visStartBesked(nuvarendespiller);
-		}
-		else{
-			nuvarendespiller = 2;
-			gc.visStartBesked(nuvarendespiller);
-		}
-		//Mens vinder ikke er fundet
-		
-		
+		//Mens vinder ikke er fundet		
 		while(!vinder) {
-			if ( nuvarendespiller == 1) {
-				nuvarendespiller = 2;
-				spilRunde(s1);
-			}
-			else {
-				spilRunde(s2);
-				nuvarendespiller = 1;
-			}
+			// finder hvilken spiller der skal kaste, og spiller en runde med denne spiller
+			nuvaerendeSpiller = spilRunde(nuvaerendeSpiller);
 			if (!vinder) {
-				GUI.getUserButtonPressed("Spiller " + nuvarendespiller + "s tur", "Tryk for at kaste!");	
+				GUI.getUserButtonPressed("Spiller " + nuvaerendeSpiller.getBrik().getSpillerNummer() + "s tur", "Tryk for at kaste!");	
 			}			
 		}
+		// while løkken er afsluttet, vinder er fundet, afslut spil
 		gc.afslutSpil();
 	}
 
-
-	public static void spilRunde(Spiller s) {
+	public static Spiller spilRunde(Spiller s) {
 
 		raflebaeger.slaaTerninger();
 		
@@ -67,17 +50,35 @@ public class SpilController {
 		flytBrik(s, raflebaeger.getSum()-1);
 		gc.visBesked(sp.getFelt(raflebaeger.getSum()-2).getBeskrivelse());
 		
-		if ( raflebaeger.getSum() == 10 ) {
-			getNuvaerendespiller( nuvarendespiller );
-		}	
 		if ( s.getBeholdning() >= 1200) {
-			getNuvaerendespiller( nuvarendespiller );
-			GUI.getUserButtonPressed("Spiller " + nuvarendespiller + " har vundet gz", "Fedt spil!");
+			getNuvaerendespiller( s );
+			GUI.getUserButtonPressed("Spiller " + s.getBrik().getSpillerNummer() + " har vundet gz", "Fedt spil!");
 			vinder = true;	
 		}
+		if ( raflebaeger.getSum() == 10 ) {
+			fjernBrik(s, raflebaeger.getSum()-1);
+			return s;
+		}	
 		fjernBrik(s, raflebaeger.getSum()-1);
+		getNuvaerendespiller(s);
+		return nuvaerendeSpiller;
+		
 	}
-
+	
+	public static Spiller hvemStarter(){
+		//afgoer hvem der starter via math.random
+		int hvemStarter = (int) (Math.random()*2)+1 ;
+		
+		if(hvemStarter == 1 ){
+			gc.visStartBesked(s1.getBrik().getSpillerNummer());
+			return s1;
+		}
+		else{
+			gc.visStartBesked(s2.getBrik().getSpillerNummer());
+			return s2;
+		}
+	}
+	
 	public static void flytBrik(Spiller s, int felt){
 		gc.placerBrik(s, felt);
 	}
@@ -86,10 +87,10 @@ public class SpilController {
 		gc.fjernBrik(s, felt);
 	}
 	
-	public static void getNuvaerendespiller ( int nuvaerendespiller ) {
-		if ( nuvaerendespiller == 1) 
-			nuvarendespiller =  2;
-		else if ( nuvaerendespiller == 2)
-			nuvarendespiller = 1;
+	public static void getNuvaerendespiller ( Spiller s ) {
+		if ( s.getBrik().getSpillerNummer() == 1) 
+			nuvaerendeSpiller = s2;
+		else if ( s.getBrik().getSpillerNummer() == 2)
+			nuvaerendeSpiller = s1;
 	}
 }
